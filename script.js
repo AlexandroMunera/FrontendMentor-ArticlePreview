4bf2e1785100f2f8e794e4c1a34d15cdfc856a86
Freud Múnera <freud.munera@qcode.co>
2021-04-05 19:12:41 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const shareButtons = document.getElementsByClassName("card__button");
+const footer = document.getElementById("card__footer");
+const footerActive = document.getElementById("card__footer-active");
+
+for (var i = 0; i < shareButtons.length; i++) {
+  shareButtons[i].addEventListener("click", showFooterActive);
+}
+
+function showFooterActive() {
+  if (footer.style.display === "none") {
+    footer.style.display = "block";
+    footerActive.style.display = "none";
+  } else {
+    footer.style.display = "none";
+    footerActive.style.display = "block";
+  }
+}
